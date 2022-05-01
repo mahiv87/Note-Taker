4950150c1437e9be10b1e39ad4cbc5b31942b7f4
Marcus Herrera <maherreraiv1987@gmail.com>
2022-05-01 10:10:43 -0600

    Added / get

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
 const readFromFile = util.promisify(fs.readFile);
