18fe2b6780c84bab0d78acc047a8f5431e630b97
Marcus Herrera <maherreraiv1987@gmail.com>
2022-05-02 11:08:41 -0600

    Added comments to server.js

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const util = require('util');
 const uuid = require('./helpers/uuid');
 const { clog } = require('./middleware/clog');
 
-
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -15,19 +14,24 @@ app.use(clog);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Static middleware for serving assets in public folder
 app.use(express.static('public'));
 
+// GET route to homepage
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
+// GET route to notes page
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
 const readFromFile = util.promisify(fs.readFile);
 
+// Function to write to json
 const writeToFile = (destination, content) =>
     fs.writeFile(destination, JSON.stringify(content, null, '\t'), (err) =>
         err ? console.error(err) : console.info(`\nData written to ${destination}`)
 );
 
+// Function to read and append json
 const readAndAppend = (content, file) => {
     fs.readFile(file, 'utf8', (err, data) => {
         if (err) {
@@ -40,10 +44,12 @@ const readAndAppend = (content, file) => {
     });
 };
 
+// GET request for notes
 app.get('/api/notes', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
+// POST request to add a note
 app.post('/api/notes', (req, res) => {
     const { title, text } = req.body;
 
@@ -61,6 +67,7 @@ app.post('/api/notes', (req, res) => {
     }
 });
 
+// Delete request to remove a specific note
 app.delete('/api/notes/:id', (req, res) => {
     readFromFile('./db/db.json').then((data) => {
         let parsedData = JSON.parse(data);
@@ -73,6 +80,7 @@ app.delete('/api/notes/:id', (req, res) => {
     .catch((err) => { if (err) {console.error(err)} });
 });
 
+// Wildcard route to direct to homepage
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
 app.listen(PORT, () => 
