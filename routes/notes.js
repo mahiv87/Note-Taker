ceb2041f4ad8fc5cf2ab18f3c12026e039926750
Marcus Herrera <maherreraiv1987@gmail.com>
2022-05-03 20:52:41 -0600

    Added code to notes route

--- routes/notes.js
+++ routes/notes.js
@@ -1,1 +1,63 @@
 const notes = require('express').Router();
+const fs = require('fs');
+const util = require('util');
+const uuid = require('../helpers/uuid');
+
+const readFromFile = util.promisify(fs.readFile);
+
+// Function to write to json
+const writeToFile = (destination, content) =>
+    fs.writeFile(destination, JSON.stringify(content, null, '\t'), (err) =>
+        err ? console.error(err) : console.info(`\nData written to ${destination}`)
+);
+
+// Function to read and append json
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            writeToFile(file, parsedData);
+        }
+    });
+};
+
+// GET request for notes
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST request to add a note
+notes.post('/', (req, res) => {
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            id: uuid(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added successfully ✅`);
+    } else {
+        res.error(`Error in adding note ❌`);
+    }
+});
+
+// Delete request to remove a specific note
+notes.delete('/:id', (req, res) => {
+    readFromFile('./db/db.json').then((data) => {
+        let parsedData = JSON.parse(data);
+        parsedData = parsedData.filter(({ id }) => id !== req.params.id);
+
+        writeToFile('./db/db.json', parsedData);
+        
+        res.json(parsedData)
+    })
+    .catch((err) => { if (err) {console.error(err)} });
+});
+
+module.exports = notes;
--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
-const util = require('util');
-const uuid = require('./helpers/uuid');
 const { clog } = require('./middleware/clog');
+const api = require('./routes/index.js');
 
 const PORT = process.env.PORT || 3001;
 
@@ -13,6 +11,7 @@ app.use(clog);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
 // Static middleware for serving assets in public folder
 app.use(express.static('public'));
@@ -22,63 +21,6 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.htm
 
 // GET route to notes page
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
-
-const readFromFile = util.promisify(fs.readFile);
-
-// Function to write to json
-const writeToFile = (destination, content) =>
-    fs.writeFile(destination, JSON.stringify(content, null, '\t'), (err) =>
-        err ? console.error(err) : console.info(`\nData written to ${destination}`)
-);
-
-// Function to read and append json
-const readAndAppend = (content, file) => {
-    fs.readFile(file, 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-        } else {
-            const parsedData = JSON.parse(data);
-            parsedData.push(content);
-            writeToFile(file, parsedData);
-        }
-    });
-};
-
-// GET request for notes
-app.get('/api/notes', (req, res) => {
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-});
-
-// POST request to add a note
-app.post('/api/notes', (req, res) => {
-    const { title, text } = req.body;
-
-    if (req.body) {
-        const newNote = {
-            title,
-            text,
-            id: uuid(),
-        };
-
-        readAndAppend(newNote, './db/db.json');
-        res.json(`Note added successfully ✅`);
-    } else {
-        res.error(`Error in adding note ❌`);
-    }
-});
-
-// Delete request to remove a specific note
-app.delete('/api/notes/:id', (req, res) => {
-    readFromFile('./db/db.json').then((data) => {
-        let parsedData = JSON.parse(data);
-        parsedData = parsedData.filter(({ id }) => id !== req.params.id);
-
-        writeToFile('./db/db.json', parsedData);
-        
-        res.json(parsedData)
-    })
-    .catch((err) => { if (err) {console.error(err)} });
-});
 
 // Wildcard route to direct to homepage
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
